56eba1bc75ec361c1207f9a39ffdc07ffb56c621
Amanda D. Clark <adc0032@auburn.edu>
2019-04-02 19:43:56 -0500

    missing a base (C) added to expression. Changed the + to a * incase nothing in between start and stop

--- find_orf.py
+++ find_orf.py
@@ -207,7 +207,7 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'AUG([GUA]{3})+UAA'
+    orf_pattern_str = r'AUG([CGUA]{3})*UAA'
     ##########################################################################
 
     # Create the regular expression object
